c732d7a6cdcb45926f6331b48a6fae8eb0f2e960
siamsarker <sarkersiam2@gmail.com>
2024-08-16 02:21:53 +0600

    basic setup done

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/user-test', function (Request $request) {
+    return ['hi' => 'test'];
+});
